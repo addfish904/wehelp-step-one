00febd3b7ac083766d42ab12440d593b84dc8489
addfish904 <rain21509517@gmail.com>
2025-02-21 17:18:18 +0800

    delete week6

--- week6/static/script.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let checkbox = document.getElementById("agree");
-    if (!checkbox.checked) {
-        alert("Please check the checkbox first");
-        return false;
-    }
-    return true;
-}
